92eaf53201dc2bb61a018d847819b4db459e4ae2
AldresLuiz <filesaldres@gmail.com>
2025-04-03 00:55:05 -0300

    Introdução a Programação - Atividade 1

---
+++ Introdução a programação/1 - introdução/main.py
@@ -0,0 +1,34 @@
+# https://github.com/AldresLuiz/Atividades
+print("Aldres Luiz")
+
+a = 3
+b = 5
+c = 9
+
+res = 2*a*3*b
+print("2*a*3*b = ",res)
+
+print("Soma de 3 variaveis = ",a+b+c)
+
+salario = 750
+aumento = 1.15
+
+print("salario + aumento : ",int(salario*aumento))
+
+metragem = float(input("Digite Um Valor Em Metros: "))
+milimetros = metragem*1000
+
+print("milimentros: ",milimetros)
+
+preco = float(input("Preço do Produto: "))
+desconto = float(input("Desconto oferecido em %: "))/100
+
+print("Produto Custa ao total ",preco," e tem um desconto de ",desconto*preco," Valor Final de ",preco*(1-desconto))
+
+salario = float(input("Digite o valor de um Salario: "))
+aumento = float(input("Digite um aumento de salario em %: "))/100
+
+print("Voçe ganhou ",salario*aumento," De aumento, novo salario : ",salario*(1+aumento))
+
+
+
